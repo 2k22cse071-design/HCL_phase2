99a58bd8c053dbf8d9e44bf2a0f50a9e66fdaf70
Kathir_Raj <2k22cse071@kiot.ac.in>
2026-02-16 15:19:52 +0530

    Update App.js

--- gitcollab/src/App.js
+++ gitcollab/src/App.js
@@ -1,17 +1,18 @@
-// import logo from './logo.svg';
 import './App.css';
-import Navbar from './nav_bar'; // Ensure using the correct filename case
+import Navbar from './nav_bar';
 import Home from './Home';
 import About from './About';
 import Contact from './Contact';
 
-import Navbar from './nav_bar';
-
 function App() {
   return (
     <div className="App">
-     <Navbar/>
+      <Navbar />
+      <Home />
+      <About />
+      <Contact />
     </div>
   );
 }
+
 export default App;
